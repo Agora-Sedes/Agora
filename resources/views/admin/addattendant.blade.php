673ded75fe0e5f5822f5ce10c4b624b5e30ed679
jeremias <jeremiasmaiasd@gmail.com>
2026-05-27 19:23:11 -0300

    formulario de incriptos de ultimo momento

--- resources/views/admin/addattendant.blade.php
+++ resources/views/admin/addattendant.blade.php
@@ -1,3 +1,15 @@
 <div>
-    <!-- Simplicity is the consequence of refined emotions. - Jean D'Alembert -->
+    <form action="" method="post">
+        @csrf
+        <label for="name">Nombre:</label>
+        <input type="text" id="name" name="name" required><br><br>
+        <label for="lastname">Apellido:</label>
+        <input type="text" id="lastname" name="lastname" required><br><br>
+        <label for="dni">DNI:</label>
+        <input type="text" id="dni" name="dni" required><br><br>
+        <label for="email">Email:</label>
+        <input type="email" id="email" name="email" required><br><br>
+        <label for="phone">Telefono:</label>
+        <input type="text" id="phone" name="phone" required><br><br>
+    </form>
 </div>
