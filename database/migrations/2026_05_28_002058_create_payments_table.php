8d73fdba270324bcd59d1c97aa24e1d391a4b564
jeremias <jeremiasmaiasd@gmail.com>
2026-05-28 09:53:15 -0300

    arreglo en el migracion de payments

--- database/migrations/2026_05_28_002058_create_payments_table.php
+++ database/migrations/2026_05_28_002058_create_payments_table.php
@@ -13,8 +13,8 @@ return new class extends Migration
     {
         Schema::create('payments', function (Blueprint $table) {
             $table->id();
-            $table->status()->enum(['done', 'pending', 'invalid']);
-            $table->method()->enum(['mercadopago', 'cash']);
+            $table->enum('status', ['done', 'pending', 'invalid'])->default('pending');
+            $table->enum('method', ['mercadopago', 'cash']);
             $table->decimal('amount', 10, 3)->unsigned();
         });
     }
