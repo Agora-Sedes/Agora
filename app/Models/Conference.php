a2510e18c319549e20a306bab17347a60a79c62f
Bowuigi <bowuigi@vern.cc>
2026-06-04 17:45:52 -0300

    feat: Models for Attendant, Conference and Talk

--- app/Models/Conference.php
+++ app/Models/Conference.php
@@ -2,9 +2,23 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Attributes\Fillable;
+use Illuminate\Database\Eloquent\Attributes\WithoutTimestamps;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
+#[Fillable(['title', 'description', 'starts_at', 'ends_at'])]
+#[WithoutTimestamps]
 class Conference extends Model
 {
-    //
+    public function attendants(): HasMany {
+        return $this->hasMany(Attendant::class)->chaperone();
+    }
+
+    protected function casts(): array {
+        return [
+            'starts_at' => 'datetime',
+            'ends_at' => 'datetime',
+        ];
+    }
 }
--- app/Models/Talk.php
+++ app/Models/Talk.php
@@ -2,9 +2,23 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Attributes\Fillable;
+use Illuminate\Database\Eloquent\Attributes\WithoutTimestamps;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Model;
 
+#[Fillable('title', 'description', 'starts_at', 'ends_at', 'speaker', 'speaker_background')]
+#[WithoutTimestamps]
 class Talk extends Model
 {
-    //
+    public function conference(): BelongsTo {
+        return $this->belongsTo(Conference::class);
+    }
+
+    protected function casts(): array {
+        return [
+            'starts_at' => 'datetime',
+            'ends_at' => 'datetime',
+        ];
+    }
 }
