49cd6e866fe8f7afece4ce744cb4246bf554cfb6
jeremias <jeremiasmaiasd@gmail.com>
2026-06-04 18:56:09 -0300

    reddirecion de las rutas de las conferencias

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\LogoutController;
 use App\Http\Middleware\AdminAuth;
+use Illuminate\Http\Request;
 
 Route::get('/inscription', [App\Http\Controllers\InscriptionController::class, 'index']);
 Route::post('/inscription', [App\Http\Controllers\InscriptionController::class, 'store']);
@@ -14,12 +15,21 @@ Route::post('/login', LoginController::class);
 Route::post('/logout', LogoutController::class)->name('logout');
 
 Route::middleware(AdminAuth::class)->group(function () {
-    Route::view('/dashboard', 'dashboard')->name('dashboard');
+Route::get('/dashboard', function (Request $request) {
+    $conferenceName = $request->query('name');
+    
+    if (!$conferenceName) {
+        return redirect('/conference-selector');
+    }
+    
+    return view('dashboard', ['conference_name' => $conferenceName]);
+})->name('dashboard');
     Route::view('/addattendant', 'admin.addattendant')->name('addattendant');
     Route::view('/attendants', 'admin.attendants')->name('attendants');
     Route::view('/qrscan', 'admin.qrscan')->name('qrscan');
     Route::view('/conference-selector', 'admin.conference-selector')->name('conference-selector');
 });
+
 
 Route::post('/buy', function (\Illuminate\Http\Request $request) {
     $quantity = $request->input('entry', 1);
