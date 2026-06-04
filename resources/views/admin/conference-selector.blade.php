36335d16c131deac4db6d511b06f5d8ad64af302
jeremias <jeremiasmaiasd@gmail.com>
2026-06-04 17:17:01 -0300

    creacion de la lista de conference

--- resources/views/admin/conference-selector.blade.php
+++ resources/views/admin/conference-selector.blade.php
@@ -1,3 +1,15 @@
 <div>
-    <!-- Breathing in, I calm body and mind. Breathing out, I smile. - Thich Nhat Hanh -->
+    <div>
+        <h2>Seleccionar Conferencia</h2>
+        <form action="" method="post">
+            @csrf
+            <label for="conference">Conferencia:</label>
+            <select id="conference" name="conference" required>
+                @foreach($conferences as $conference)
+                    <option value="{{ $conference->id }}">Tu mama es mi novia</option>
+                @endforeach
+            </select><br><br>
+            <button type="submit">Seleccionar Conferencia</button>
+        </form>
+    </div><!-- Breathing in, I calm body and mind. Breathing out, I smile. - Thich Nhat Hanh -->
 </div>
