3c046d509d7312600baa33964624aecea829a6db
Juan Cruz Vila <parlantephilips7@gmail.com>
2026-06-08 23:18:05 -0300

    refactor(no-ticket): remove build from install-dependencies.sh

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -27,11 +27,6 @@ echo "
 npm install --ignore-scripts || exit 1
 
 echo "
---- Building initial bundle ---
-"
-npm run build || exit 1
-
-echo "
 --- Generating development .env configuration ---
 "
 
@@ -39,8 +34,8 @@ cp .env.example .env
 php artisan key:generate --ansi
 
 echo "
- +---------------------------------------------+
- | Done! Reverse this process with:            |
- |   rm -r node_modules/ public/build/ vendor/ |
- +---------------------------------------------+
+ +-------------------------------------+
+ | Done! Reverse this process with:    |
+ |   rm -r node_modules/ vendor/       |
+ +-------------------------------------+
 "
