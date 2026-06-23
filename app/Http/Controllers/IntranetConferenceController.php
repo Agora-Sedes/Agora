405d96da5531efbc1c4df1597539ce4fd403c119
jeremias <jeremiasmaiasd@gmail.com>
2026-06-22 23:18:31 -0300

    colocacion de conferecia no encontrada y de la lista

--- app/Http/Controllers/IntranetConferenceController.php
+++ app/Http/Controllers/IntranetConferenceController.php
@@ -2,18 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Conference;
+
 class IntranetConferenceController extends Controller
 {
     public function list()
     {
-        // TODO: Database integration
-        return view('intranet.conferences.list');
+        $conferences = Conference::all()->sortByDesc('starts_at');
+
+        return view('intranet.conferences.list', [
+            'conferences' => $conferences,
+        ]);
     }
 
     public function dashboard(int $id)
     {
-        // TODO: Database integration
-        return view('intranet.conferences.dashboard');
+        $conference = Conference::find($id);
+
+        if (is_null($conference)) {
+            return response('Conferencia no encontrada', 404);
+        }
+
+        return view('intranet.conferences.dashboard', [
+            'conference' => $conference,
+        ]);
     }
 
     public function qrScan(int $id)
