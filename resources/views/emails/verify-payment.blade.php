449b9510e9cb6e20edfadc4b5fa7c85fe7550473
Bowuigi <bowuigi@vern.cc>
2026-07-17 22:36:02 -0300

    Stop disclosing internal information on the payment verification email

--- resources/views/emails/verify-payment.blade.php
+++ resources/views/emails/verify-payment.blade.php
@@ -9,11 +9,7 @@
     <h1>Verificá tu pago en efectivo</h1>
 
     <p>
-        Mostrá este código QR a quien recibe el pago al ingresar a la jornada.
-    </p>
-
-    <p>
-        El QR contiene datos cifrados con el usuario, la conferencia y el pago para poder buscar al inscripto en la base de datos correspondiente.
+        Mostrá este código QR a quien recibe el pago al ingresar a la jornada:
     </p>
 
     <div style="text-align: center; margin: 30px 0;">
@@ -26,7 +22,7 @@
     </div>
 
     <p style="font-size: 12px; color: #666;">
-        Inscripto: {{ $attendant->full_name }} | Conferencia ID: {{ $attendant->conference_id }}
+        Inscripto: {{ $attendant->full_name }}
     </p>
 
 </body>
